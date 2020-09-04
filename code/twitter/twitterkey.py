93850de32bd9bf8b962d8982e4d3a4fc85fab2f8
girishsukukumar <girish_kumar@yahoo.com>
2020-09-04 11:32:06 +0530

    Create twitterkey.py

---
+++ code/twitter/twitterkey.py
@@ -0,0 +1,4 @@
+consumer_key = "abcd145454r45454"
+consumer_secret_key = "dsfsdfdsferwe32432mldceoriw"
+AccessToken ="mlkf495-4poweokdlsmdflksdnfoerhroiewrksflsdflj"
+AccessTokenSecret = "qwnkmnsiqw4093204893utryut43uknfvkfdnfklerwkpoei4i"
